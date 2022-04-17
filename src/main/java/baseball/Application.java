8a4f66851fb351fbd9aec7d9afa240e2c0474914
Hyoun Keun <hyounkeun2009@gmail.com>
2022-04-18 00:02:38 +0900

    feature: Add playing BullsAndCowsGame to Application main method.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import baseball.bullsAndCows.application.BullsAndCowsGameService;
+import baseball.bullsAndCows.domain.GuideMessage;
+import baseball.bullsAndCows.domain.Opponent;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    private static BullsAndCowsGameService bullsAndCowsGameService;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        bullsAndCowsGameService = new BullsAndCowsGameService(new Opponent().pickAnswerNumber());
+        do {
+            GuideMessage.숫자입력_안내.printMessage();
+        }
+        while (bullsAndCowsGameService.play(Console.readLine()));
     }
+
 }
